32e460abe05470ae15f9553db906a81ec43bbd21
Laura Yien <lnyien@ucdavis.edu>
2023-03-15 23:41:58 -0700

    finished version 2

--- final/version2/script.js
+++ final/version2/script.js
@@ -1,61 +1,90 @@
-(function (){
+(function () {
     "use strict"
+    alert("Scroll down to view Laura's Top 5 Boba Places")
     console.log("JS Running");
-// scroll function
-    window.addEventListener("load", function(){
+    // scroll function
+    window.addEventListener("load", function () {
         const posts = document.querySelectorAll("article");
         let postTops = [];
         let pageTop;
         let counter = 0;
         let previousCounter = 0;
-        let doneResizing; 
+        let doneResizing;
 
-         posts.forEach( function(post){
-            postTops.push(Math.floor(post.getBoundingClientRect().top+window.pageYOffset));
+        posts.forEach(function (post) {
+            postTops.push(Math.floor(post.getBoundingClientRect().top + window.pageYOffset));
         });
 
-        window.addEventListener("scroll", function(){
-            pageTop = Math.floor(window.pageYOffset)-650; //at top of page, 0 pixels, more and more pixels are above top of the window
+        window.addEventListener("scroll", function () {
+            pageTop = Math.floor(window.pageYOffset) - 650; //at top of page, 0 pixels, more and more pixels are above top of the window
             console.log(pageTop);
-            if(pageTop > postTops[counter]){
+            if (pageTop > postTops[counter]) {
                 console.log(postTops[counter])
                 counter++;
                 console.log(`scrolling down ${counter}`);
-            } else if (counter > 1 && pageTop < postTops[counter-1]) {
+            } else if (counter > 1 && pageTop < postTops[counter - 1]) {
                 counter--;
                 console.log(`scrolling up ${counter}`)
                 //scrolling into the next higher section 
             }
 
             const thisDiv = document.querySelector(`.targetDiv${counter}`);
-            const postitnote= document.querySelector('.postitnote');
+            const postitnote = document.querySelector('.postitnote');
 
-            
-            if(counter != previousCounter){
+
+            if (counter != previousCounter) {
                 const thisDiv = document.querySelector(`.targetDiv${counter}`);
                 console.log(counter);
                 console.log(thisDiv);
                 thisDiv.classList.replace("hidden", "show");
                 previousCounter = counter;
             }
-
-
-            // if(counter == 1){
-            //     console.log(counter);
-            //     console.log(thisDiv);
-            //     thisDiv.classList.replace("hidden", "show1");
-            //     previousCounter = counter;
-            // } else if (counter == 2){
-            //     thisDiv.classList.replace("hidden2", "show1");
-            // } else if (counter == 3) {
-            //     thisDiv.classList.replace("hidden", "show1");
-            // } else{
-            //     thisDiv.classList.replace("hidden", "show1");
-            // }
-
         });
 
+
+
+
     });
+
+    // const bobaimages = document.querySelectorAll(".second img");
+    //     const background = document.querySelectorAll("article");
+    //     console.log(bobaimages);
+    //     console.log(background);
+    
+    //     for (const eachImg of bobaimages) {
+    //         eachImg.addEventListener("mouseover", function () {
+    //             if (eachImg == 0) {
+    //                 background[1].className = "blueanimation";
+    //             }
+    //             else if (eachImg.ID == 'tpercentbobaimg'){
+    //                 background.className = "blueanimation";
+    //             }  
+    //             else if (eachImg.ID == 'umeteabobaimg'){
+    //                 background.className = "blueanimation";
+    //             }
+    //             else if (eachImg.ID == 'teaspoonbobaimg'){
+    //                 background.className = "blueanimation";
+    //             }
+    //             else if (eachImg.ID == 'iteabobaimg'){
+    //                 background.className = "blueanimation";
+    //             }
+    //         });
+    
+    //         eachImg.addEventListener("mouseout", function () {
+    //             if(eachImg.ID == 'T4bobaimg' || 'tpercentbobaimg' || 'umeteabobaimg' || 'teaspoonbobaimg' || 'iteabobaimg' ){
+    //                 background.removeAttribute("class");
+    //             }
+    //         });
+    
+    //     }
+
+
+
+    
+
+
+
+
 
     // smoothscroll back to top 
     const button = document.querySelector("#button");
@@ -66,18 +95,18 @@
 
 
 
-    function smoothScroll(event){
+    function smoothScroll(event) {
         event.preventDefault();
 
         const targetID = event.target.getAttribute("href"); //if can get each attribute can know which section scroll to
         const targetAnchor = document.querySelector(targetID);
-        const originalTop = Math.floor(targetAnchor.getBoundingClientRect().top)-200; //rounds down value & stops 200 px from the top of the page 
-        window.scrollBy({top:originalTop, left:0, behavior: "smooth"})
+        const originalTop = Math.floor(targetAnchor.getBoundingClientRect().top) - 200; //rounds down value & stops 200 px from the top of the page 
+        window.scrollBy({ top: originalTop, left: 0, behavior: "smooth" })
         console.log(originalTop);
     };
 
-  
-     
+
+
 
 })();
 
